be7704e92e7bc18289f6612970a6e239a864ba19
Renan Rendon <falecom@renan.inf.br>
2019-02-08 17:40:56 -0200

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb(){
+        const categories = [
+            { id: 1, name: 'Moradia', description: 'Pagamento de contas da casa'},
+            { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios'},
+            { id: 3, name: 'Lazer', description: 'Cinema, praia, parques, etc'},
+            { id: 4, name: 'Salário', description: 'Recebimento de salário'}
+        ]
+
+        return { categories }
+    }
+}
